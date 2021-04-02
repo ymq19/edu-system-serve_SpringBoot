8dcc2e7145b41ab0a22e625900843d54a2b2eb7f
ymq19 <1976583473@qq.com>
2021-04-02 12:18:42 +0800

    '项目创建'

---
+++ src/main/java/edu/system/serve/ServeApplication.java
@@ -0,0 +1,13 @@
+package edu.system.serve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/system/serve/ServeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.system.serve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
